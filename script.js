ff40f70a60452e7b804725f7154bf30cabb514a1
Braab144 <Daniel.Helmut00@gmail.com>
2026-02-03 13:21:05 +0100

    Add getComputerChoice Function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-print("Test");
+getComputerChoice();
+
+function getComputerChoice(){
+
+    let computerChoice;
+
+    switch(Math.trunc(Math.random() * 3)){
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissor";
+            break;
+    }
+
+    console.log(computerChoice);
+}
